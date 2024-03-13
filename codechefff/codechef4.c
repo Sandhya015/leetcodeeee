50aa7b02341760237c51c28744055c5fdb05bee9
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-03-13 21:00:40 +0530

    Create codechef4.c

---
+++ codechefff/codechef4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    int maxSpeed;
+    scanf("%d", &maxSpeed);
+
+    if (maxSpeed <= 40) {
+        printf("NO\n");
+    } else {
+        printf("YES\n");
+    }
+
+    return 0;
+}
