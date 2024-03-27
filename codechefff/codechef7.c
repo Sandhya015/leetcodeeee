fa4a5cfe9a227d290cf2631338226fe6fd4c322e
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-03-27 21:26:28 +0530

    Create codechef7.c

---
+++ codechefff/codechef7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+    scanf("%d", &day);
+
+    if (day == 15) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
